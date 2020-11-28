e7b7f471f1a6e8fe4e952433eb288f05647b9fa4
Ardian Alphita <ardianpramudya81@gmail.com>
2020-11-29 01:11:43 +0700

    Redesign Profile

--- application/views/layouts/master.blade.php
+++ application/views/layouts/master.blade.php
@@ -12,7 +12,7 @@
         get_css('fonts.css');
     @endphp
 </head>
-<body class="bg-randomize">
+<body style="background-color: #F2EEE8;">
     @yield('content')
     
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
