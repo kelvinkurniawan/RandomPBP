b81300be155157dc3594d7a754a3c9c77361e414
Kelvin Kurniawan O <tepinnko@gmail.com>
2020-12-01 06:13:47 +0700

    exporting new database

--- random3.sql
+++ random3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 29, 2020 at 03:33 PM
+-- Generation Time: Dec 01, 2020 at 12:13 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -25,69 +25,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `attachments`
---
-
-CREATE TABLE `attachments` (
-  `id` int(11) NOT NULL,
-  `path` varchar(255) NOT NULL,
-  `user` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `likes`
---
-
-CREATE TABLE `likes` (
-  `id` int(11) NOT NULL,
-  `user` int(11) NOT NULL,
-  `post` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `likes`
---
-
-INSERT INTO `likes` (`id`, `user`, `post`) VALUES
-(16, 3, 3),
-(17, 3, 5),
-(18, 3, 11);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `posts`
---
-
-CREATE TABLE `posts` (
-  `id` int(11) NOT NULL,
-  `body` text NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
-  `user` int(11) NOT NULL,
-  `parent` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `posts`
---
-
-INSERT INTO `posts` (`id`, `body`, `timestamp`, `user`, `parent`) VALUES
-(1, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', '2020-11-28 17:59:52', 3, 0),
-(2, 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy.', '2020-11-28 18:00:52', 3, 1),
-(3, 'Aku sayang kamu tapi kamu enggak', '2020-11-28 19:00:26', 3, 0),
-(5, 'berjuang lama tapi #siasia', '2020-11-28 21:29:08', 3, 0),
-(6, '#jadi gimana ?', '2020-11-28 21:29:21', 3, 0),
-(7, '@ardian apa kabar', '2020-11-28 21:41:11', 3, 0),
-(8, '@ardian #what', '2020-11-28 22:52:07', 3, 0),
-(9, 'ngomong apa sih bambang', '2020-11-28 22:52:52', 3, 1),
-(10, 'ya ampun .. @ardian dimana', '2020-11-28 22:55:07', 3, 1),
-(11, 'bacot ngobrol sendiri', '2020-11-28 23:00:32', 3, 10);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `users`
 --
 
@@ -95,6 +32,7 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `username` varchar(30) NOT NULL,
   `password` varchar(255) NOT NULL,
   `birth` date DEFAULT NULL,
   `photo` varchar(255) DEFAULT NULL,
@@ -105,30 +43,13 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `birth`, `photo`, `role`) VALUES
-(3, 'Kelvin Kurniawan Oktavianto', 'tepinnko@gmail.com', '$2y$10$9qGzPwFbRkaEjW6wZPfrJODz5UC94Np9LHR8YHPW0usc9/2SS8V0.', NULL, NULL, 0);
+INSERT INTO `users` (`id`, `name`, `email`, `username`, `password`, `birth`, `photo`, `role`) VALUES
+(3, 'Kelvin Kurniawan Oktavianto', 'tepinnko@gmail.com', 'kelvink', '$2y$10$9qGzPwFbRkaEjW6wZPfrJODz5UC94Np9LHR8YHPW0usc9/2SS8V0.', NULL, 'photo.png', 0),
+(4, 'Ardian Pramudya', 'ardianpramudya81@gmail.com', '', '$2y$10$lDniUB9857sz13Pg3pzqkOaZyRKB73N4tElF4Z1/d.Cfm2Qk9Z2Ru', NULL, 'cio.png', 0);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `attachments`
---
-ALTER TABLE `attachments`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `likes`
---
-ALTER TABLE `likes`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `posts`
---
-ALTER TABLE `posts`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -141,28 +62,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `attachments`
---
-ALTER TABLE `attachments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `likes`
---
-ALTER TABLE `likes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
-
---
--- AUTO_INCREMENT for table `posts`
---
-ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
